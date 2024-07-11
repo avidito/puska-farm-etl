7b8e820822c784889e57c2ca9c7efd0dfbce6b7a
avidito <av.leonardif@gmail.com>
2024-07-11 13:27:22 +0700

    feat(populasi): add source data for masuk/keluar/lahir/mati

--- stream/etl/seq_fact_populasi/modules/entity.py
+++ stream/etl/seq_fact_populasi/modules/entity.py
@@ -37,6 +37,48 @@ class HistoryPopulasi(BaseModel):
     jml_perah_anakan_jantan: int
     jml_perah_anakan_betina: int
 
+class HistoryKelahiranKematian(BaseModel):
+    tgl_pencatatan: date
+    id_peternak: int
+    jml_lahir_pedaging_jantan: int
+    jml_lahir_pedaging_betina: int
+    jml_lahir_perah_jantan: int
+    jml_lahir_perah_betina: int
+    jml_mati_pedaging_jantan: int
+    jml_mati_pedaging_betina: int
+    jml_mati_pedaging_anakan_jantan: int
+    jml_mati_pedaging_anakan_betina: int
+    jml_mati_perah_jantan: int
+    jml_mati_perah_betina: int
+    jml_mati_perah_anakan_jantan: int
+    jml_mati_perah_anakan_betina: int
+
+class PencatatanTernakKeluar(BaseModel):
+    tgl_pencatatan: date
+    id_peternak: int
+    jenis_mitra_penerima: str
+    jml_pedaging_jantan: int
+    jml_pedaging_betina: int
+    jml_pedaging_anakan_jantan: int
+    jml_pedaging_anakan_betina: int
+    jml_perah_jantan: int
+    jml_perah_betina: int
+    jml_perah_anakan_jantan: int
+    jml_perah_anakan_betina: int
+
+class PencatatanTernakMasuk(BaseModel):
+    tgl_pencatatan: date
+    id_peternak: int
+    jenis_mitra_pengirim: str
+    jml_pedaging_jantan: int
+    jml_pedaging_betina: int
+    jml_pedaging_anakan_jantan: int
+    jml_pedaging_anakan_betina: int
+    jml_perah_jantan: int
+    jml_perah_betina: int
+    jml_perah_anakan_jantan: int
+    jml_perah_anakan_betina: int
+
 
 # Kafka
 class KafkaPopulasi(BaseModel):
--- stream/etl/seq_fact_populasi/modules/usecase.py
+++ stream/etl/seq_fact_populasi/modules/usecase.py
@@ -2,8 +2,11 @@ from typing import Dict
 from datetime import date
 
 from etl.seq_fact_populasi.modules.entity import (
-    HistoryPopulasi,
     FactPopulasi,
+    HistoryPopulasi,
+    HistoryKelahiranKematian,
+    PencatatanTernakMasuk,
+    PencatatanTernakKeluar,
 )
 from etl.seq_fact_populasi.modules.repository import (
     FactPopulasiDWHRepository,
@@ -89,6 +92,105 @@ class FactPopulasiUsecase:
         
         fact_populasi.jumlah = new_jumlah
         return fact_populasi
+    
+    def transform_kelahiran_kematian(self, new_lahir_mati: HistoryKelahiranKematian, fact_populasi: FactPopulasi) -> FactPopulasi:
+        tipe_ternak = fact_populasi.tipe_ternak
+        jenis_kelamin = fact_populasi.jenis_kelamin
+        tipe_usia = fact_populasi.tipe_usia
+
+        if tipe_ternak == "Pedaging":
+            if jenis_kelamin == "Jantan":
+                if tipe_usia == "Anakan":
+                    new_lahir = new_lahir_mati.jml_lahir_pedaging_jantan
+                    new_mati = new_lahir_mati.jml_mati_pedaging_anakan_jantan
+                elif tipe_usia == "Dewasa":
+                    new_lahir = 0
+                    new_mati = new_lahir_mati.jml_mati_pedaging_jantan
+            elif jenis_kelamin == "Betina":
+                if tipe_usia == "Anakan":
+                    new_lahir = new_lahir_mati.jml_lahir_pedaging_betina
+                    new_mati = new_lahir_mati.jml_mati_pedaging_anakan_betina
+                elif tipe_usia == "Dewasa":
+                    new_lahir = 0
+                    new_mati = new_lahir_mati.jml_mati_pedaging_betina
+        elif tipe_ternak == "Perah":
+            if jenis_kelamin == "Jantan":
+                if tipe_usia == "Anakan":
+                    new_lahir = new_lahir_mati.jml_lahir_perah_jantan
+                    new_mati = new_lahir_mati.jml_mati_perah_anakan_jantan
+                elif tipe_usia == "Dewasa":
+                    new_lahir = 0
+                    new_mati = new_lahir_mati.jml_mati_perah_jantan
+            elif jenis_kelamin == "Betina":
+                if tipe_usia == "Anakan":
+                    new_lahir = new_lahir_mati.jml_lahir_perah_betina
+                    new_mati = new_lahir_mati.jml_mati_perah_anakan_betina
+                elif tipe_usia == "Dewasa":
+                    new_lahir = 0
+                    new_mati = new_lahir_mati.jml_mati_perah_betina
+        
+        fact_populasi.jumlah_lahir = new_lahir
+        fact_populasi.jumlah_mati = new_mati
+    
+    def transform_masuk(self, new_masuk: PencatatanTernakMasuk, fact_populasi: FactPopulasi) -> FactPopulasi:
+        tipe_ternak = fact_populasi.tipe_ternak
+        jenis_kelamin = fact_populasi.jenis_kelamin
+        tipe_usia = fact_populasi.tipe_usia
+
+        if tipe_ternak == "Pedaging":
+            if jenis_kelamin == "Jantan":
+                if tipe_usia == "Anakan":
+                    new_masuk = new_masuk.jml_pedaging_anakan_jantan
+                elif tipe_usia == "Dewasa":
+                    new_masuk = new_masuk.jml_pedaging_jantan
+            elif jenis_kelamin == "Betina":
+                if tipe_usia == "Anakan":
+                    new_masuk = new_masuk.jml_pedaging_anakan_betina
+                elif tipe_usia == "Dewasa":
+                    new_masuk = new_masuk.jml_pedaging_betina
+        elif tipe_ternak == "Perah":
+            if jenis_kelamin == "Jantan":
+                if tipe_usia == "Anakan":
+                    new_masuk = new_masuk.jml_perah_anakan_jantan
+                elif tipe_usia == "Dewasa":
+                    new_masuk = new_masuk.jml_perah_betina
+            elif jenis_kelamin == "Betina":
+                if tipe_usia == "Anakan":
+                    new_masuk = new_masuk.jml_perah_anakan_betina
+                elif tipe_usia == "Dewasa":
+                    new_masuk = new_masuk.jml_perah_betina
+        
+        fact_populasi.jumlah_masuk = new_masuk
+    
+    def transform_keluar(self, new_keluar: PencatatanTernakKeluar, fact_populasi: FactPopulasi) -> FactPopulasi:
+        tipe_ternak = fact_populasi.tipe_ternak
+        jenis_kelamin = fact_populasi.jenis_kelamin
+        tipe_usia = fact_populasi.tipe_usia
+
+        if tipe_ternak == "Pedaging":
+            if jenis_kelamin == "Jantan":
+                if tipe_usia == "Anakan":
+                    new_keluar = new_keluar.jml_pedaging_anakan_jantan
+                elif tipe_usia == "Dewasa":
+                    new_keluar = new_keluar.jml_pedaging_jantan
+            elif jenis_kelamin == "Betina":
+                if tipe_usia == "Anakan":
+                    new_keluar = new_keluar.jml_pedaging_anakan_betina
+                elif tipe_usia == "Dewasa":
+                    new_keluar = new_keluar.jml_pedaging_betina
+        elif tipe_ternak == "Perah":
+            if jenis_kelamin == "Jantan":
+                if tipe_usia == "Anakan":
+                    new_keluar = new_keluar.jml_perah_anakan_jantan
+                elif tipe_usia == "Dewasa":
+                    new_keluar = new_keluar.jml_perah_betina
+            elif jenis_kelamin == "Betina":
+                if tipe_usia == "Anakan":
+                    new_keluar = new_keluar.jml_perah_anakan_betina
+                elif tipe_usia == "Dewasa":
+                    new_keluar = new_keluar.jml_perah_betina
+        
+        fact_populasi.jumlah_keluar = new_keluar
 
 
     def load(self, fact_populasi: FactPopulasi):
